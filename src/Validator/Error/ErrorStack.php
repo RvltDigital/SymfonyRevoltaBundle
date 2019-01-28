0cd13dfc84a49cf56c058b56f200224e19649807
Dominik Chrastecky <dominik@chrastecky.cz>
2019-01-28 17:14:37 +0100

    make error stack iterable

--- src/Validator/Error/ErrorStack.php
+++ src/Validator/Error/ErrorStack.php
@@ -2,8 +2,14 @@
 
 namespace RvltDigital\SymfonyRevoltaBundle\Validator\Error;
 
-class ErrorStack implements \Countable
+class ErrorStack implements \Countable, \Iterator
 {
+
+    /**
+     * For the Iterator interface
+     * @var int
+     */
+    private $i = 0;
 
     /**
      * @var Error[]
@@ -74,5 +80,61 @@ class ErrorStack implements \Countable
             /** @var Error $error */
             return $error->getMessage();
         }, $this->errors);
+    }
+
+    /**
+     * Return the current element
+     * @link https://php.net/manual/en/iterator.current.php
+     * @return Error Can return any type.
+     * @since 5.0.0
+     */
+    public function current()
+    {
+        return $this->errors[$this->i];
+    }
+
+    /**
+     * Move forward to next element
+     * @link https://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function next()
+    {
+        ++$this->i;
+    }
+
+    /**
+     * Return the key of the current element
+     * @link https://php.net/manual/en/iterator.key.php
+     * @return int scalar on success, or null on failure.
+     * @since 5.0.0
+     */
+    public function key()
+    {
+        return $this->i;
+    }
+
+    /**
+     * Checks if current position is valid
+     * @link https://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     * @since 5.0.0
+     */
+    public function valid()
+    {
+        return array_key_exists($this->i, $this->errors);
+    }
+
+    /**
+     * Rewind the Iterator to the first element
+     * @link https://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function rewind()
+    {
+        $this->i = 0;
     }
 }
