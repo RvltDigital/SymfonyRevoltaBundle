4365d27335c6358deee4ea733bd6148182b202af
Dominik Chrastecky <dominik@chrastecky.cz>
2019-05-14 17:37:40 +0200

    fix apache authorization header problem

---
+++ src/Listener/FixApacheAuthorizationHeaderListener.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace RvltDigital\SymfonyRevoltaBundle\Listener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class FixApacheAuthorizationHeaderListener implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::REQUEST => 'addAuthorizationHeader',
+        ];
+    }
+
+    public function addAuthorizationHeader(GetResponseEvent $event)
+    {
+        $headers = $event->getRequest()->headers;
+        if (!$headers->has('Authorization') && function_exists('getallheaders')) {
+            /** @noinspection PhpComposerExtensionStubsInspection */
+            $rawHeaders = getallheaders();
+            foreach ($rawHeaders as $key => $value) {
+                if (strtolower($key) === 'authorization') {
+                    $headers->set('Authorization', $value);
+                }
+            }
+        }
+    }
+}
